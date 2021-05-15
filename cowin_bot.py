7dc7ea487d92820a09d5c7b587c2ec721c271eac
Jebin Philipose <jebinphilip24@gmail.com>
2021-05-16 00:56:09 +0530

    Add get_district_id function

--- cowin_bot.py
+++ cowin_bot.py
@@ -17,3 +17,16 @@ def get_state_id(state_name):
             if state['state_name'] == state_name:
                 state_id = state['state_id']
     return state_id
+
+
+def get_district_id(state_id, district_name):
+    district_name = district_name.title()
+    district_id = None
+    url = f'https://cdn-api.co-vin.in/api/v2/admin/location/districts/{state_id}'
+    response = requests.get(url, headers=browser_header)
+    if response.ok and ('districts' in response.text):
+        districts = json.loads(response.text)['districts']
+        for district in districts:
+            if district['district_name'] == district_name:
+                district_id = district['district_id']
+    return district_id
